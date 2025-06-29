0f9668485ee616da1262f74ab70e06944b30bf00
Albertusprg <myalbertusprg@gmail.com>
2025-06-29 23:30:46 +0700

    perbaikan deployment

--- src/app/api/posts/[id]/route.ts
+++ src/app/api/posts/[id]/route.ts
@@ -4,6 +4,7 @@ import { BlogPost } from '../endpoints';
 
 export async function GET(
   request: NextRequest,
+
   { params }: { params: Promise<{ id: string }> }
 ) {
   const { id } = await params;
--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -4,6 +4,7 @@ import { BlogPost } from '../../posts/endpoints';
 
 export async function GET(
   request: NextRequest,
+
   { params }: { params: Promise<{ id: string }> }
 ) {
   const { id } = await params;
